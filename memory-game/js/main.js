26ee63a95702f42012da47a2a788b5dd47eac457
Zain Sattar <zainsattar18@Zains-MBP.home>
2020-04-02 15:28:29 -0400

    created game logic

---
+++ memory-game/js/main.js
@@ -0,0 +1,13 @@
+
+
+let cardOne = "queen";
+
+let cardTwo = "queen";
+
+let cardThree = "king";
+
+let cardFour = "king";
+
+console.log("User flipped " + cardOne);
+
+console.log("User flipped " + cardThree);
